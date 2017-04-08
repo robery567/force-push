233d9e73e4ed243b1765effe6af1b2a207b88803
Robert Colca <robery_office@yahoo.ro>
2017-04-08 17:34:53 +0300

    Added /get/consultants route and functionality

--- api/src/controllers.php
+++ api/src/controllers.php
@@ -14,18 +14,83 @@ $app->get('/', function () use ($app) {
     ->bind('homepage');
 
 $app->get('/get/specializations', function () use ($app) {
-    $sql = "SELECT * FROM specializations ORDER BY name ASC";
+    $sql = "SELECT * FROM `specializations` ORDER BY `name` ASC";
     $data = $app['db']->fetchAll($sql);
 
     return $app->json($data);
-}) -> bind('get_specializations');
+})->bind('get_specializations');
 
 $app->get('/get/counties', function () use ($app) {
-    $sql = "SELECT * FROM counties ORDER BY name ASC";
+    $sql = "SELECT * FROM `counties` ORDER BY `name` ASC";
     $data = $app['db']->fetchAll($sql);
 
     return $app->json($data);
-}) -> bind('get_counties');
+})->bind('get_counties');
+
+$app->get('/get/consultants', function (Request $request) use ($app) {
+    $sql = "SELECT 
+              `c`.`id` AS `id`,
+              `c`.`name` AS `name`,
+              `c`.`legitimation` AS `legitimation`,
+              `c`.`telephone`
+              FROM `consultants` `c`
+              JOIN `consultants_specializations` `cs`
+                ON `cs`.`parent_id` = `c`.`id`
+              JOIN `specializations` `s`
+                ON `s`.`id` = `cs`.`specialization_id`
+              JOIN `consultants_county` `cc`
+                ON `cc`.`parent_id` = `c`.`id`
+              JOIN `counties`
+                ON `counties`.`id` = `cc`.`county`
+              WHERE 1
+            ";
+
+    $specialization = (int)$request->query->get('specialization');
+    $county = (int)$request->query->get('county');
+    $offset = (int)$request->query->get('start');
+    $count = (int)$request->query->get('count');
+
+    $sqlParameters = array();
+    if (!empty($specialization)) {
+        $sql .= " AND `cs`.`specialization_id` = ?";
+        array_push($sqlParameters, $specialization);
+    }
+
+    if (!empty($county)) {
+        $sql .= " AND `cc`.`county` = ?";
+        array_push($sqlParameters, $county);
+    }
+
+    if (!empty($offset) && !empty($count)) {
+        $sql .= " LIMIT {$offset}, {$count}";
+    }
+
+    $data = $app['db']->fetchAll($sql, $sqlParameters);
+
+    foreach ($data as $consultandId => $consultantData) {
+        $sql = "SELECT 
+                        `c`.`id` AS `id`,
+                        `c`.`name` AS `name` 
+                        FROM consultants_county `cc`
+                        JOIN `counties` `c`
+                          ON `c`.`id` = `cc`.`county`
+                        WHERE `cc`.`parent_id` = ?";
+
+        $data[$consultandId]['counties'] = $app['db']->fetchAll($sql, array((int)$consultantData['id']));
+
+        $sql = "SELECT 
+                        `s`.`id` AS `id`,
+                        `s`.`name` AS `name` 
+                        FROM `consultants_specializations` `cs`
+                        JOIN `specializations` `s`
+                          ON `s`.`id` = `cs`.`specialization_id`
+                        WHERE `cs`.`parent_id` = ?";
+
+        $data[$consultandId]['specializations'] = $app['db']->fetchAll($sql, array((int)$consultantData['id']));
+    }
+
+    return $app->json($data);
+});
 
 $app->after(function (Request $request, Response $response) {
     $response->headers->set('Access-Control-Allow-Origin', '*');
